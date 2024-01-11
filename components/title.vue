979509fa5ff26cf3fdd73e6e77f438262d078dd4
MaloGerard01 <malo.gerard35@gmail.com>
2024-01-11 15:29:19 +0100

    quisommesnous page

---
+++ components/title.vue
@@ -0,0 +1,20 @@
+<template>
+      <div class="mb-12"> 
+            <h2 class="text-vertPale font-bold uppercase text-sm">{{title}}</h2>
+          <h3 class="text-black font-bold text-3xl">{{subtitle}}</h3>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  subtitle: {
+    type: String,
+    required: true,
+  },
+});
+const { review } = props;
+</script>
--- pages/quisommesnous.vue
+++ pages/quisommesnous.vue
@@ -5,7 +5,7 @@
   
     <body>
       <div class="h-screen flex">
-        <div class="flex flex-row justify-center items-center ">
+        <div class="flex flex-row justify-end items-center ">
           <div class="flex flex-col gap-6 w-[60%]">
             <h1 class="text-vertPale font-bold text-4xl">Un conseiller à ton écoute !</h1>
           </div>
@@ -16,154 +16,164 @@
 
   
   
-      <div class="h-screen  flex bg-white">
-        <div class="flex flex-row-reverse justify-start gap-6 items-center w-screen w-1/2">
-          <div class="flex flex-col gap-6 w-[45%] mb-20">
-            <h2 class="text-vertPale font-bold uppercase text-sm">Youto c'est quoi ?</h2>
-            <h3 class="text-black font-bold text-3xl">Ta plateforme d'aide préférée !</h3>
-            <p class="text-sm w-[65%]">
-              Remplir des documents est souvent une corvée !
-              On se perds facilement dans la tonne de documents qu'il faut remplir,
-              on ne sait pas réellement ce que les organismes attendent de nous...
-              <br/><strong>Youto est la pour TOI !</strong><br/>
-              Notre objectif est de rendre cette tache presque agréable à tes yeux,
-              compte sur nous.
-            </p>
+      <div class="h-screen px-[11rem]  flex bg-lightOrange">
+        <div class="flex flex-row justify-start gap-6 items-center w-screen w-1/2">
+          <div class="flex flex-col w-full mb-20">
+            <h2 class="text-melonOrange font-bold uppercase text-sm">Lorem ipsum</h2>
+            <h3 class="text-black font-bold text-3xl mb-12">Quelques chiffres pour parler de Youto</h3>
   
-            <div class="flex justify-end">
-              <div class="flex flex-col absolute right-[14rem]">
-                <div class="flex flex-row justify-end gap-4">
-                  <div class="mt-4 w-50 shadow-md block px-4 py-3 bg-vertPale text-white rounded-2xl">
-                    <p class="text-center uppercase font-bold">Sécurité</p>
-                    <p class="text-center font-sm">Prenez contact avec nos conseillers <br /> afin de leurs expliquer votre
-                      problème</p>
+            <div class="">
+              <div class="flex flex-col gap-6">
+                <div class="flex flex-row gap-10">
+                  <div class="mt-4 w-1/4 shadow-md block p-6 bg-white rounded-2xl">
+                    <p class=" uppercase font-bold uppercase text-melonOrange">1 millions d’utilisateurs satisfaits</p>
+                    <p class=" pt-2 text-sm">Youto a déjà aidé des milliers d’utilisateurs à traverser leurs démarches administratives avec succès.</p>
+                  </div>
+                  <div class="mt-4 w-1/4 shadow-md block p-6 bg-white rounded-2xl ">
+                    <p class=" uppercase font-bold uppercase text-melonOrange">145 heures économisées</p>
+                    <p class=" pt-2 text-sm">En utilisant Youto, nos utilisateurs ont économisé collectivement des centaines d’heures.</p>
+                  </div>
+                  <div class="mt-4 w-1/4 shadow-md block p-6 bg-white rounded-2xl">
+                    <p class=" uppercase font-bold uppercase text-melonOrange">55 démarches simplifiées</p>
+                    <p class=" pt-2 text-sm">Youto simplifie chaque étape des démarches administratives, en fournissant des guides détaillés, des modèles de formulaires…</p>
                   </div>
                 </div>
-  
-                <div class="flex flex-row gap-4">
-                  <div class="mt-4 w-50 shadow-md block px-4 py-3 bg-vertPale text-white rounded-2xl">
-                    <p class="text-center uppercase font-bold">Accompagnement</p>
-                    <p class="text-center font-sm">Prenez contact avec nos conseillers <br /> afin de leurs expliquer votre
-                      problème</p>
+
+                <div class="flex flex-row justify-end gap-10">
+                  <div class="mt-4 w-1/4 shadow-md block p-6 bg-white rounded-2xl">
+                    <p class=" uppercase font-bold uppercase text-melonOrange">14 experts qualifiés</p>
+                    <p class=" pt-2 text-sm">Youto s’appuie sur une équipe d’experts qualifiés, garantissant ainsi des informations précises et à jour.</p>
                   </div>
-                  <div class="mt-4 w-50 shadow-md block px-4 py-3 bg-vertPale text-white rounded-2xl">
-                    <p class="text-center uppercase font-bold">Sérénité</p>
-                    <p class="text-center font-sm">Prenez contact avec nos conseillers <br /> afin de leurs expliquer votre
-                      problème</p>
+                  <div class="mt-4 w-1/4 shadow-md block p-6 bg-white rounded-2xl">
+                    <p class=" uppercase font-bold uppercase text-melonOrange">99 % de réussite des démarches</p>
+                    <p class=" pt-2 text-sm">Grâce à l’assistance de Youto, nos utilisateurs ont obtenu un taux de réussite élevé dans leurs démarches administratives.</p>
+                  </div>
+                  <div class="mt-4 w-1/4 shadow-md block p-6 bg-white rounded-2xl">
+                    <p class=" uppercase font-bold uppercase text-melonOrange">4 partenaires institutionnels</p>
+                    <p class=" pt-2 text-sm">Youto collabore avec des partenaires institutionnels de confiance pour garantir l’exactitude des informations.</p>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <img class="absolute left-24 w-1/2 z-1" src="~/assets/to.png"/>
         </div>
       </div>
   
-      <div class="h-screen  flex bg-lightOrange">
-        <div class="flex flex-row justify-center gap-6 items-center w-screen">
-          <div class="flex flex-col gap-6">
-            <h2 class="text-vertPale font-bold uppercase text-sm">Avis clients</h2>
-            <h3 class="text-black font-bold text-3xl">Ils sont tous d'accord</h3>
-            <p class="text-sm">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <br />
-              do eiusmod tempor incididunt ut labore et dolore magna <br />
-              aliqua. Ut enim ad minim veniam, quis nostrud exercitation <br />
-              ullamco laboris nisi ut aliquip ex ea commodo consequat. <br />
-              Duis aute irure dolor in reprehenderit in voluptate velit esse <br />
-              cillum dolore
-            </p>
-          </div>
-          <div class="flex flex-col gap-6">
-            test
-          </div>
-        </div>
-      </div>
+      <div class="px-[11rem] my-12">
+      <div class="mb-12"> 
+            <h2 class="text-vertPale font-bold uppercase text-sm">Partenaires</h2>
+          <h3 class="text-black font-bold text-3xl">Ils nous font confiance</h3>
+    </div>
+      <Swiper
+      :height="800"
+      :modules="[SwiperAutoplay]"
+      :slides-per-view="6"
+      :space-between="40"
+      :loop="true"
+      :autoplay="{
+        delay: 8000,
+        disableOnInteraction: true
+      }"
+    >
+      <SwiperSlide
+        v-for="(partner, idx) in partners"
+        :key="idx"
+      >
+      <img class="" :src="`_nuxt/assets/` + partner.url">
+    </SwiperSlide>
 
+      <!-- useSwiper() within a swiper instance -->
+      <SwiperControls />
+    </Swiper>
+</div>
 
-
-      <div class="h-full flex z-10 mb-4">
-        <div class="swiper-container mx-60 overflow-x-scroll">
-          <div class="mb-12"> 
+      <div class="px-[11rem] my-12">
+      <div class="mb-12"> 
             <h2 class="text-vertPale font-bold uppercase text-sm">Articles de blog</h2>
           <h3 class="text-black font-bold text-3xl">Ils parlent de nous</h3>
-                  <div class="swiper-pagination"></div>
-                  <div class="swiper-button-prev">test</div>
-                  <div class="swiper-button-next">test</div>
-          </div>
-          
-          <div class="swiper-wrapper flex justify-center">
-            <div class="swiper-slide " v-for="(card, index) in cards" :key="index">
-              <div class="bg-vertPale  shadow-md rounded-2xl  ">
+    </div>
+      <Swiper
+      :height="800"
+      :modules="[SwiperAutoplay]"
+      :slides-per-view="3"
+      :space-between="40"
+      :loop="false"
+      :autoplay="{
+        delay: 8000,
+        disableOnInteraction: true
+      }"
+    >
+      <SwiperSlide
+        v-for="(card, idx) in cards"
+        :key="idx"
+      >
+      <div class="bg-vertPale  shadow-md rounded-2xl  ">
                 <img class=" rounded-t-2xl" src="~/assets/cardtest2.webp">
                 <div class="px-8 py-4 flex flex-col gap-3"> 
                   <h3 class="text-white font-bold text-left">{{ card.title }}</h3>
-                <p class="text-sm text-white">{{ card.description }}</p>
+                <p class="text-sm text-white my-6">{{ card.description }}</p>
                 <div class="flex justify-end">
-                  <NuxtLink class="text-white mt-1 w-1/3 text-sm text-center cursor-pointer block py-2">
+                  <NuxtLink class="text-white mt-1 w-1/3 text-sm text-center cursor-pointer block py-2 font-bold">
                     Voir plus >
                   </NuxtLink>
                 </div>
                 </div>
               </div>
-            </div>
-          </div>
-        </div>
-      </div>
-  
+      </SwiperSlide>
+
+      <!-- useSwiper() within a swiper instance -->
+      <SwiperControls />
+    </Swiper>
+</div>
     </body>
   </template>
   
   <script setup>
-  import Swiper from 'swiper';
   import { useAuthStore } from '~/stores/auth'; // import the auth store we just created
   const { userInfo } = storeToRefs(useAuthStore()); // make authenticated state reactive with storeToRefs
   import { onMounted } from 'vue';
-  
-  var cards = [
-    { title: 'Simplifiez vos démarches administratives en un clic', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
-    { title: 'Le compagnon idéal pour vos démarches administratives', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
-    { title: 'La solution numérique qui simplifie vos relations avec l’administration', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
-    { title: 'La solution numérique qui simplifie vos relations avec l’administration', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
 
-    // Ajoutez d'autres cartes au besoin
-  ];
-  var swiper = null;
-  
-  onMounted(() => {
-    swiper = new Swiper('.swiper-container', {
-      slidesPerView: 3,
-      spaceBetween: 30,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-  });
-  // beforeDestroy() {
-  //     if (this.swiper) {
-  //       this.swiper.destroy();
-  //     }
-  //   };
-  
+  var partners = [
+  { url: 'caf.png'},
+  { url: 'pe.png'},
+  { url: 'fnaim.png'},
+  { url: 'impots.png'},
+  { url: 'caf.png'},
+  { url: 'pe.png'},
+  { url: 'fnaim.png'},
+]
+
+
+  var cards = [
+  { title: 'Simplifiez vos démarches administratives en un clic !', description: 'Dites adieu aux tracas des démarches administratives grâce à Youto, la plateforme révolutionnaire !' },
+  { title: 'Le compagnon idéal pour vos démarches administratives', description: 'La vie est trop courte pour passer des heures à remplir des formulaires et à attendre dans les bureaux administratifs.' },
+  { title: 'La solution numérique qui simplifie vos relations avec l’administration', description: 'Les longues files d’attente et les formulaires interminables appartiennent désormais au passé.' },
+  { title: 'La solution numérique qui simplifie vos relations avec l’administration', description: 'Les longues files d’attente et les formulaires interminables appartiennent désormais au passé.' },
+  // Ajoutez d'autres cartes au besoin
+]
   </script>
   
   
   <style>
   /* Styles spécifiques au carousel */
-  .swiper-container {
-    width: 100%;
-    padding-top: 20px;
-  }
-  
   .swiper-slide {
-    border-radius: 8px;
-  }
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.swiper-wrapper {
+  min-width: 100vh;
+  width: 100vh;
+}
+.swiper-cards {
+  width: 240px;
+  height: 240px;
+}
+.swiper-cards .swiper-slide {
+  border-radius: 6px;
+  border: 1px solid black;
+}
   
-  .half-bg {
-    background: linear-gradient(to bottom, #f39454 50%, white 0%);
-  }</style>
+  
+  </style>
   
